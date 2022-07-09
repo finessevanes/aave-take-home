9ecd16a1a10f8edf19abcfadb128860033877a3b
Vanessa <vanessa.mercado24@gmail.com>
2022-07-09 15:28:04 -0700

    updated contract- broken :/

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,20 +1,20 @@
-import React from 'react'
 import { ethers } from 'ethers'
 import { ButtonStyle } from './styles'
 
-const Dashboard = () => {
-  // pool abi
-  const PoolV3Artifact = require('@aave/core-v3/artifacts/contracts/protocol/pool/Pool.sol/Pool.json')
-  
-  // van...ado.eth public key
-  const PUBLIC_KEY = '0x7EEa9F4A69c3a43d333366Efc0798523910b146D'
-  
-  // AVAX-TestnetMintableERC20-Avalanche
-  const AVAX_ADDRESS = '0x407287b03D1167593AF113d32093942be13A535f'
-  
-  // Fuji V3 Pool Contract
-  const POOL_CONTRACT_ADDRESS = '0x794a61358D6845594F94dc1DB02A252b5b4814aD'
+// van...ado.eth public key
+const PUBLIC_KEY = '0x7EEa9F4A69c3a43d333366Efc0798523910b146D'
 
+const Dashboard = () => {
+  // mainnet Pool abi
+  const PoolV3Artifact = require('@aave/core-v3/artifacts/contracts/protocol/pool/Pool.sol/Pool.json')
+
+  // AVAX-TestnetMintableERC20-Avalanche
+  // https://docs.aave.com/developers/deployed-contracts/v3-testnet-addresses
+  const AVAX_ADDRESS_ERC20 = '0x407287b03D1167593AF113d32093942be13A535f'
+
+  // Fuji V3 Pool-Proxy-Avalanche
+  // https://docs.aave.com/developers/deployed-contracts/v3-testnet-addresses
+  const POOL_CONTRACT_ADDRESS = '0xb47673b7a73D78743AFF1487AF69dBB5763F00cA'
 
   async function supplyFunds() {
     try {
@@ -22,25 +22,32 @@ const Dashboard = () => {
       const provider = new ethers.providers.Web3Provider(ethereum)
       const signer = provider.getSigner()
       const connectedContract = new ethers.Contract(POOL_CONTRACT_ADDRESS, PoolV3Artifact.abi, signer)
-
-      // USDC-TestnetMintableERC20-Avalanche = 0x3E937B4881CBd500d05EeDAB7BA203f2b7B3f74f
-      //  = 0x407287b03D1167593AF113d32093942be13A535f
-
+      let testt = provider.getCode(POOL_CONTRACT_ADDRESS)
+      console.log(testt)
 
       // supply the contract : supply(address: assetToBeSupplied, uint256: amount, address: onBehaldOf, uint16: referralCode)
       let supplyTxn = await connectedContract.supply(
-        AVAX_ADDRESS,
+        AVAX_ADDRESS_ERC20,
         1,
         PUBLIC_KEY,
-        0
+        0,
+        {
+          gasLimit: 3e6
+        }
       )
 
+      console.log('minting!')
+
       await supplyTxn.wait()
+
+      console.log('your transaction')
+      console.log(supplyTxn)
 
     } catch (e) {
       console.log(e)
     }
   }
+
 
   return (
     <div className='flex flex-col'>
