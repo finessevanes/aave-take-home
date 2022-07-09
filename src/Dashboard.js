2efa6ea706731730a71f6e055bf1019b3bbb2909
Vanessa <vanessa.mercado24@gmail.com>
2022-07-09 16:14:22 -0700

    wip

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -6,19 +6,24 @@ import { ButtonStyle } from './styles'
 const PUBLIC_KEY = '0x7EEa9F4A69c3a43d333366Efc0798523910b146D'
 
 const Dashboard = () => {
-  // mainnet Pool abi
-  const PoolV3Artifact = require('@aave/core-v3/artifacts/contracts/protocol/pool/Pool.sol/Pool.json')
+  // _______ AAVE V3 FUJI CONTRACTS _______
+  // https://docs.aave.com/developers/deployed-contracts/v3-testnet-addresses
+  const POOL_CONTRACT = '0xb47673b7a73D78743AFF1487AF69dBB5763F00cA'
+  const UI_POOL_PROVIDER_V3_CONTRACT = '0x1D01f7d8B42Ec47837966732f831E1D6321df499'
+  const WALLET_BALANCE_PROVIDER_CONTRACT = '0xd19443202328A66875a51560c28276868B8C61C2'
 
-  // AVAX-TestnetMintableERC20-Avalanche
+  // _______ MAINNET ABIs _______
+  const PoolV3ABI = require('@aave/core-v3/artifacts/contracts/protocol/pool/Pool.sol/Pool.json')
+  const UiPoolDataProviderV3ABI = require('@aave/periphery-v3/artifacts/contracts/misc/UiPoolDataProviderV3.sol/UiPoolDataProviderV3.json')
+  const WalletBalanceProviderABI = require('@aave/periphery-v3/artifacts/contracts/misc/WalletBalanceProvider.sol/WalletBalanceProvider.json')
+
+  // _______ TestnetMintableERC20 _______
+  // AVAX-Avalanche
   // https://docs.aave.com/developers/deployed-contracts/v3-testnet-addresses
   const AVAX_ADDRESS_ERC20 = '0x407287b03D1167593AF113d32093942be13A535f'
 
-  // Fuji V3 Pool-Proxy-Avalanche
-  // https://docs.aave.com/developers/deployed-contracts/v3-testnet-addresses
-  const POOL_CONTRACT_ADDRESS = '0xb47673b7a73D78743AFF1487AF69dBB5763F00cA'
-
-  useEffect(()=>{
-    getUSDCBalance()
+  useEffect(() => {
+    getAVAXBalance()
   }, [])
 
   async function supplyFunds() {
@@ -26,11 +31,10 @@ const Dashboard = () => {
       const { ethereum } = window
       const provider = new ethers.providers.Web3Provider(ethereum)
       const signer = provider.getSigner()
-      const connectedContract = new ethers.Contract(POOL_CONTRACT_ADDRESS, PoolV3Artifact.abi, signer)
-      let testt = provider.getCode(POOL_CONTRACT_ADDRESS)
-      console.log(testt)
+      const connectedContract = new ethers.Contract(POOL_CONTRACT, PoolV3ABI.abi, signer)
 
       // supply the contract : supply(address: assetToBeSupplied, uint256: amount, address: onBehaldOf, uint16: referralCode)
+      // https://docs.aave.com/developers/core-contracts/pool
       let supplyTxn = await connectedContract.supply(
         AVAX_ADDRESS_ERC20,
         1,
@@ -41,22 +45,30 @@ const Dashboard = () => {
         }
       )
 
-      console.log('minting!')
-
+      console.log('... transaction ...')
       await supplyTxn.wait()
-
-      console.log('your transaction')
-      console.log(supplyTxn)
+      console.log('completed: ', supplyTxn)
 
     } catch (e) {
       console.log(e)
     }
   }
 
-  async function getUSDCBalance() {
+
+  async function getAVAXBalance() {
     try {
-      console.log('the balance here')
-    } catch(e){
+      const { ethereum } = window
+      const provider = new ethers.providers.Web3Provider(ethereum)
+      const signer = provider.getSigner()
+      const connectedContract = new ethers.Contract(WALLET_BALANCE_PROVIDER_CONTRACT, WalletBalanceProviderABI.abi, signer)
+
+      // https://docs.aave.com/developers/periphery-contracts/uipooldataproviderv3#getreservesdata
+      let balanceOfTxn = await connectedContract.balanceOf(PUBLIC_KEY, AVAX_ADDRESS_ERC20)
+
+      
+
+      console.log('AVAX Balance: ', balanceOfTxn.toNumber())
+    } catch (e) {
       console.log(e)
     }
   }
@@ -65,7 +77,12 @@ const Dashboard = () => {
   return (
     <div className='flex flex-col'>
       <div>Dashboard</div>
-      <button onClick={supplyFunds} className={`${ButtonStyle} + mt-3`}>add funds</button>
+      <button
+      onClick={supplyFunds}
+      className={`${ButtonStyle} + mt-3`}
+      >
+        Add Funds
+      </button>
     </div>
   )
 }
